3526250287b7d339e180b599747918b9c13f153e
Sammei <91269085+vaibhavkapriyal@users.noreply.github.com>
2022-04-13 11:56:20 +0530

    Create mergesort.c

---
+++ mergesort.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+void merge(int a[],int l,int mid,int h)
+{
+	int i=l,j=mid+1,k=0;
+	int b[h-l+1];
+	while(i<=mid && j<=h)
+	{
+		if(a[i]<a[j])
+			b[k++]=a[i++];
+		else
+			b[k++]=a[j++];
+	}
+	while(i<=mid)
+	{
+		b[k++]=a[i++];
+	}
+	while(j<=h)
+	{
+		b[k++]=a[j++];
+	}
+	int y=0;
+	for(int x=l;x<=h;x++)
+	{
+		a[x]=b[y];
+		y++;
+	}
+}
+
+void mergesort(int a[],int l,int h)
+{
+	if(l<h)
+	{
+		int mid=(l+h)/2;
+		mergesort(a,l,mid);
+		mergesort(a,mid+1,h);
+		merge(a,l,mid,h);
+	}
+}
+
+int main()
+{
+	int n,i;
+	printf("ENter the no. of elements of the arary\n");
+	scanf("%d",&n);
+	int a[n];
+	printf("Enter the elements of the array\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	mergesort(a,0,n-1);
+	printf("Sorted Array:- \n");
+	
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	return 0;
+	
+}
